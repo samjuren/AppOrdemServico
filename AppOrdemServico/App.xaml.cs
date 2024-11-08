04d2a25a8e18afbdd9953d02354e2945c271945a
Samuel Juren <sammjureen@outlook.com>
2024-11-07 21:48:03 -0300

    Login View, ViewModel e criado uma ViewModelBase

--- AppOrdemServico/App.xaml.cs
+++ AppOrdemServico/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppOrdemServico
+﻿using AppOrdemServico.View.Login;
+
+namespace AppOrdemServico
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new LoginView());
         }
     }
 }
---
+++ AppOrdemServico/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OnetCleaning.ViewModel.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //private PopUpModalLoad PopUpModalLoad { get; set; } = new PopUpModalLoad();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ViewModelBase()
+        {
+            PropertyChanged += ViewModelBase_PropertyChanged;
+        }
+
+        private async void ViewModelBase_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(IsBusy))
+            {
+                await ShowLoadingView();
+            }
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set 
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        protected async Task ShowLoadingView()
+        {
+            try
+            {
+                // Page? page = NavigationHelpers.GetCurrentPage();
+                //
+                // if (IsBusy)
+                // {
+                //     PopUpModalLoad = new PopUpModalLoad();
+                //     page?.ShowPopupAsync(PopUpModalLoad);
+                // }
+                // else
+                // {
+                //     PopUpModalLoad?.CloseAsync();
+                //     PopUpModalLoad = null;
+                // }
+            }
+            catch (Exception ex)
+            {
+                string teste = "";
+            }
+        }
+    }
+}
